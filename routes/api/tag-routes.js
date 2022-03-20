a44c1ea94fb36c8f72cebea8bb47997a817e48ab
elizabeth berube <elizabethdberube@gmail.com>
2022-03-20 09:22:12 -0400

    finished routes. Need to test everything

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,98 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll(req.params.id, {
+
+      include: [{ model: Product }]
+
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Product.findByPk(req.params.id, {
+
+      include: [{ model: Product }]
+    });
+    res.status(200).json(tagData);
+    if (!productData) {
+      res.status(404).json({ message: 'No tag with that id!' });
+      return;
+    }
+    res.status(404).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const TagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(TagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(tag);
+    })
+    .then((TagIds) => res.status(200).json(TagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body,
+      {
+        where: {
+          id: req.body.id,
+        },
+      });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const catagoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!catagoryData) {
+      res.status(404).json({ message: 'No catagonry with that id!' });
+      return;
+    }
+    res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const config = require('config.json');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize/types');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+startMySql();
+
+async function startMySql() {
+  const { host, port, user, database } = config.database;
+  const connection = await mysql.createConnection({ host, port, user, database });
+
+  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+  db.User = require('./routes')(sequelize);
+  await sequelize.sync();
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 app.use(routes);
 
@@ -16,3 +31,7 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
 });
+
+
+
+module.exports = db = {};
